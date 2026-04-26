40f746d00742dd3d585bfd5626ea5481e0c300c4
kaziSariaRahman <sariarahaman@gmail.com>
2026-04-27 01:28:11 +0600

    Added main function initially

--- main.cpp
+++ main.cpp
@@ -1,1 +1,18 @@
+#include <GL/glut.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+int main(int argc,char**argv){
+    srand((unsigned)time(NULL));
+    glutInit(&argc,argv);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
+    glutInitWindowSize(WIN_W,WIN_H);
+    glutInitWindowPosition(100,50);
+    glutCreateWindow("DX Ball - CSE 426");
+    openGLInit();
+     glutMainLoop();
+    return 0;
+}
